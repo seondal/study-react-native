3b76f83a9fec09d3a82bee151e4d3f9ee532121d
seondal <sseondal@gmail.com>
2021-11-07 21:38:34 +0900

    4.1.2 클래스 스타일링

--- chap04-style/App.js
+++ chap04-style/App.js
@@ -1,38 +1,34 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColorColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text
-        style={{
-          padding: 10,
-          fontSize: 26,
-          fontWeight: "600",
-          color: "black",
-        }}
-      >
-        Inline Styling - Text
-      </Text>
-      <Text
-        style={{
-          padding: 10,
-          fontSize: 26,
-          fontWeight: "400",
-          color: "red",
-        }}
-      >
-        Inline Styling - Error
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Inline Styling - Text</Text>
+      <Text style={styles.error}>Inline Styling - Error</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    padding: 10,
+    fontSize: 26,
+    fontWeight: "600",
+    color: "black",
+  },
+  error: {
+    padding: 10,
+    fontSize: 26,
+    fontWeight: "400",
+    color: "red",
+  },
+});
 
 export default App;
